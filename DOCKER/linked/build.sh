5f9acb36635e473cacf8cd45c7caff9e743d0d2f
Zach Ramsay <zach@coinculture.info>
2015-08-09 20:06:23 +0000

    shell script for testing #17, works for two nodes

--- DOCKER/linked/build.sh
+++ DOCKER/linked/build.sh
@@ -1,10 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 set -e
 
 # mct = mint client test
 
 cd $GOPATH/src/github.com/eris-ltd/mint-client
 echo "********** BUILDING TENDERMINT ********"
+
 docker build -t mct_tendermint -f ./DOCKER/linked/DockerfileTendermint . 
 echo "********** BUILDING ERIS-KEYS ********"
 docker build -t mct_keys -f ./DOCKER/linked/DockerfileKeys . 
@@ -12,4 +13,4 @@ echo "********** BUILDING MINT-CLIENT ********"
 docker build -t mct_client -f ./DOCKER/linked/DockerfileClient . 
 
 cd ./DOCKER/linked
-./run.sh
+bash run.sh
