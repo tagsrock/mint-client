d15c89e893f59d234e63ed6a8f23f74b9d24cba3
Ethan Buchman <ethan@coinculture.info>
2015-10-09 18:22:15 +0000

    use docker machine for testing

---
+++ DOCKER/linked/params.sh
@@ -0,0 +1,9 @@
+
+# path in the GOPATH
+export base=github.com/eris-ltd/mint-client
+
+# scripts for building containers and running dependency containers
+export build_script=DOCKER/linked/build.sh
+
+# we use this for building and running the base
+export docker_latest="1.8.2"
