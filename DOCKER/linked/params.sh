5f7b5d04f71de20f63649e5d2cdd5b39a4f07a65
Ethan Buchman <ethan@coinculture.info>
2015-10-10 03:52:51 +0000

    add integration tests

--- DOCKER/linked/params.sh
+++ DOCKER/linked/params.sh
@@ -7,3 +7,6 @@ export build_script=DOCKER/linked/build.sh
 
 # we use this for building and running the base
 export docker_latest="1.8.2"
+
+# we want this repo to trigger integration tests when we push to "staging"
+export integration_tests_branch=staging
