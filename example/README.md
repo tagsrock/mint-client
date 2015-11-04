ac10abc8e7343c0de3e04696a97de85392ddfe22
Ethan Buchman <ethan@coinculture.info>
2015-11-04 13:01:03 +0200

    improve example

---
+++ example/README.md
@@ -0,0 +1,13 @@
+Low-Level Example
+-----------------
+
+The `example.sh` is an executable tutorial demonstrating how the pieces of the tendermint/mint-client stack fit together at the lowest level, using curl requests.
+
+Run `example.sh` to try the example. Note it will attempt to install all the tools.
+
+If the tools are already installed, use `NO_INSTALL=true bash example.sh`.
+
+Clean up before subsequent calls by running `kill.sh`.
+
+The script works on a MacOsX Mountain Lion, and requires some modifications to work on Linux.
+
--- example/example.sh
+++ example/example.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # This file is a complete demonstration for the following steps in the eris pipeline
 # using nothing but "curl" to talk to HTTP servers and standard unix commands for processing:
 #
@@ -9,7 +11,7 @@
 #  5) Wait for a confirmation
 #  6) Ensure the contract was deployed correctly
 #  7) Create, sign, broadcast, and wait for transaction that talks to the contract
-#  8) Be Master of the Universe!!!!
+#  8) Query the contract without sending a transaction
 
 
 ########################################
@@ -24,20 +26,22 @@
 
 # now install the eris tools:
 
-# install the keys daemon for signing transactions
-#go get github.com/eris-ltd/eris-keys
+if [[ "$NO_INSTALL" == "" ]]; then
+	# install the keys daemon for signing transactions
+	go get github.com/eris-ltd/eris-keys
 
-# install the mint-client tools
-#go get github.com/eris-ltd/mint-client/...
+	# install the mint-client tools
+	go get github.com/eris-ltd/mint-client/...
 
-# install the erisdb (requires branch 0.10.3)
-#git clone https://github.com/eris-ltd/erisdb $GOPATH/src/github.com/eris-ltd/erisdb
-#cd $GOPATH/src/github.com/eris-ltd/erisdb
-#git checkout 0.10.3
-#go install ./cmd/erisdb
+	# install the erisdb (requires branch 0.10.3)
+	git clone https://github.com/eris-ltd/erisdb $GOPATH/src/github.com/eris-ltd/erisdb
+	cd $GOPATH/src/github.com/eris-ltd/erisdb
+	git checkout 0.10.3
+	go install ./cmd/erisdb
 
-# install the eris-abi tool
-#go get github.com/eris-ltd/eris-abi/cmd/eris-abi
+	# install the eris-abi tool (required for formatting transactions to contracts)
+	go get github.com/eris-ltd/eris-abi/cmd/eris-abi
+fi
 
 ########################################
 ## Step 1 - Start a chain	      ##
@@ -59,10 +63,11 @@ erisdb ~/.eris/blockchains/$CHAIN_ID  &> ~/.eris/blockchains/$CHAIN_ID/log &
 # start the keys server (for signing transactions)
 eris-keys server &
 
+# let everything start up
 sleep 2
 
 # import the validator's private key into the key server
-# NOTE: this converts the tendermint private ket format to the eris private key format
+# NOTE: this converts the tendermint private key format to the eris private key format
 # This step will be eliminated in the near future as the validator's come to use the eris key format
 ADDRESS=`mintkey eris ~/.eris/blockchains/$CHAIN_ID/priv_validator.json`
 
@@ -89,7 +94,7 @@ EOM
 CODE64=`echo $CODE | base64`
 
 # json data for the curl request to the compile server
-read -r -d '' REQ << EOM
+read -r -d '' JSON_DATA << EOM
 {
 	"name":"mycontract",
 	"language":"sol",
@@ -100,10 +105,10 @@ EOM
 # location of compiler
 URL="https://compilers.eris.industries:8091/compile"
 
-# curl that baby!
-RESULT=`curl --silent -X POST -d "${REQ}" $URL --header "Content-Type:application/json"`
+# compile that baby!
+RESULT=`curl --silent -X POST -d "${JSON_DATA}" $URL --header "Content-Type:application/json"`
 
-# fetch fields from the json result
+# the compile server returns the bytecode (in base64) and the abi (json)
 BYTECODE=`echo $RESULT | jq .bytecode`
 ABI=`echo $RESULT | jq .abi`
 
@@ -111,13 +116,15 @@ ABI=`echo $RESULT | jq .abi`
 BYTECODE="${BYTECODE%\"}"
 BYTECODE="${BYTECODE#\"}"
 
-# get the byte code back in hex
+# convert bytecode to hex
 # NOTE: this works on mac, but base64 is slightly different on linux (need -d rather than -D)
 BYTECODE=`echo $BYTECODE | base64 -D | hexdump -ve '1/1 "%.2X"'`
 
-
+# unescape quotes in the json and write the ABI to file
+# TODO: fix the lllc-server so this doesn't happen
 ABI=`eval echo $ABI` 
 ABI=`echo $ABI | jq .`
+echo $ABI > add.abi
 
 echo "BYTE CODE:"
 echo $BYTECODE
@@ -127,14 +134,11 @@ echo "ABI:"
 echo $ABI
 echo ""
 
-# save the ABI to a file for use later
-echo $ABI > add.abi
-
-
 #################################################################
 ## Step 3 - Create and Sign Transaction for Deploying Contract ##
 #################################################################
 
+# location of the blockchain node's rpc server
 ERISDB_HOST="localhost:46657"
 
 # to create the transaction, we need to know the account's nonce, so we fetch from the blockchain using simple HTTP
@@ -168,6 +172,7 @@ echo ""
 
 
 # to sign the SIGN_BYTES, we curl the eris-keys server:
+# (we gave it the private key for this address at the beginning - with mintkey)
 read -r -d '' REQ << EOM
 {
 	"msg":"$SIGN_BYTES_HEX",
@@ -185,11 +190,12 @@ echo "SIGNATURE:"
 echo $SIGNATURE
 echo ""
 
-# we're goingi to need the pubkey
-# (this can also be done as a curl request to eris-keys but the signing example above should be sufficiently illustrative)
+# we're going to need the pubkey
+# (the pubkey can also be fetched via a curl request to $ERIS_KEYS_HOST/pub with post body {"addr:"$ADDRESS"}
 PUBKEY=`eris-keys pub --addr=$ADDRESS`
 
 # now we can actually construct the transaction (it's just the sign bytes plus the pubkey and signature!)
+# since it's a CallTx with an empty address, a new contract will be created from the data (the bytecode)
 read -r -d '' CREATE_CONTRACT_TX << EOM
 [$CALLTX_TYPE, {
 	"input":{
@@ -249,11 +255,14 @@ echo "BLOCKHEIGHT"
 echo "$BLOCKHEIGHT"
 echo ""
 
+# Note we could also set up a websocket connection and subscribe to NewBlock events
+# (eg. subscribeAndWait in github.com/eris-ltd/mint-client/mintx/core/core.go )
+
 #############################################
 ## Step 6 - Verify the contract's bytecode ##
 #############################################
 
-CODE=`curl -X GET 'http://'"$ERISDB_HOST"'/get_account?address='"$CONTRACT_ADDRESS"'' --silent | jq ."result"[1].account.code`
+CODE=`curl -X GET 'http://'"$ERISDB_HOST"'/get_account?address="'"$CONTRACT_ADDRESS"'"' --silent | jq ."result"[1].account.code`
 
 # strip quotes
 CODE="${CODE%\"}"
@@ -265,6 +274,7 @@ echo ""
 
 # NOTE: CODE won't be exactly equal to BYTECODE 
 # because BYTECODE contains additional code for the actual deployment (the init/constructor sequence of a contract)
+# so we only ensure that BYTECODE contains CODE
 if [[ "$BYTECODE" == *"$CODE"* ]]; then
 	echo "THE CODE WAS DEPLOYED CORRECTLY!"
 else
@@ -282,7 +292,7 @@ FEE=0
 GAS=1000
 AMOUNT=1
 
-# we are going to call the add function of our contract
+# we are going to call the "add" function of our contract
 # and use it to add two numbers
 FUNCTION="add"
 ARG1="25"
@@ -292,6 +302,7 @@ SUM_EXPECTED=$(( $ARG1 + $ARG2 ))
 # we need to format the data for the abi properly
 # this part is tricky because we need to get the function identifier for the function we are trying to call from the contract
 # the function identifier is the first 4 bytes of the sha3 hash of a canonical form of the function signature
+# details are here: https://github.com/ethereum/wiki/wiki/Ethereum-Contract-ABI
 # we use the eris-abi tool to make this simple:
 DATA=`eris-abi pack --input file add.abi $FUNCTION $ARG1 $ARG2`
 
@@ -299,11 +310,10 @@ echo "DATA FOR CONTRACT CALL:"
 echo $DATA
 echo ""
 
-# since we've already shown how to create a transaction, sign it, and send it to the blockchain using curl
-# now we do it simply using the mintx tool:
+# since we've already shown how to create a transaction, sign it, and send it to the blockchain using curl,
+# now we do it simply using the mintx tool.
 # the --sign and --broadcast flags ensure the transaction is signed (by the private key associated with --pubkey)
-# and broadcast to the chain
-# the --wait flag waits until the transaction is confirmed
+# and broadcast to the chain. the --wait flag waits until the transaction is confirmed
 RESULT=`mintx call --node-addr=$ERISDB_HOST --chainID=$CHAIN_ID --to=$CONTRACT_ADDRESS --amt=$AMOUNT --fee=$FEE --gas=$GAS --data=$DATA --pubkey=$PUBKEY --sign --broadcast --wait`
 
 echo "$RESULT"
@@ -316,14 +326,39 @@ SUM_GOT=`echo "$RESULT" | grep "Return Value:" | awk '{print $3}' | sed 's/^0*//
 SUM_GOT=`echo $((16#$SUM_GOT))`
 
 
-
 if [[ "$SUM_GOT" != "$SUM_EXPECTED" ]]; then
-	echo "SMART CONTRACT ADDITION FAILED"
+	echo "SMART CONTRACT ADDITION TX FAILED"
 	echo "GOT $SUM_GOT"
 	echo "EXPECTED $SUM_EXPECTED"
 else
-	echo "SMART CONTRACT ADDITION SUCCEEDED!"
+	echo "SMART CONTRACT ADDITION TX SUCCEEDED!"
 	echo "$ARG1 + $ARG2 = $SUM_GOT"
 fi
+echo ""
 
 
+##################################################################
+## Step 8 - Talk to Contracts Without Creating Transactions     ##
+##################################################################
+
+# It is possible to "query" contracts using the /call endpoint.
+# Such queries are only "simulated calls", in that there is no transaction (or signature)
+# required, and hence they have no effect on the blockchain state.
+
+SUM_GOT=`curl -X GET 'http://'"$ERISDB_HOST"'/call?fromAddress="'"$ADDRESS"'"&toAddress="'"$CONTRACT_ADDRESS"'"&data="'"$DATA"'"' --silent | jq ."result"[1].return`
+
+# strip quotes
+SUM_GOT="${SUM_GOT%\"}"
+SUM_GOT="${SUM_GOT#\"}"
+
+# convert it from hex to int
+SUM_GOT=`echo $((16#$SUM_GOT))`
+
+if [[ "$SUM_GOT" != "$SUM_EXPECTED" ]]; then
+	echo "SMART CONTRACT ADDITION QUERY FAILED"
+	echo "GOT $SUM_GOT"
+	echo "EXPECTED $SUM_EXPECTED"
+else
+	echo "SMART CONTRACT ADDITION QUERY SUCCEEDED!"
+	echo "$ARG1 + $ARG2 = $SUM_GOT"
+fi
