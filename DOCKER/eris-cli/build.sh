62013a0b8f8b0a4fea50c693d3aee2af76e09402
Ethan Buchman <ethan@coinculture.info>
2015-11-27 03:23:50 +0000

    version eris/eris in test

--- DOCKER/eris-cli/build.sh
+++ DOCKER/eris-cli/build.sh
@@ -4,10 +4,17 @@ set -e
 # mct = mint client test
 
 REPO=github.com/eris-ltd/mint-client
+if [[ "$ERIS_VERSION" == "" ]]; then
+	ERIS_VERSION=0.11
+fi
 
 cd $GOPATH/src/github.com/eris-ltd/mint-client
 echo "********** BUILDING MINT-CLIENT ********"
 docker build -t mct_client -f ./DOCKER/linked/DockerfileClient . 
 
+docker run --name eris-data eris/data echo "Data-container for testing with eris-cli"
+
+docker cp $GOPATH/src/$REPO/ eris-data:/home/eris/.eris/mint-client/
+
 # eris/eris container in which we run the tests
-docker run -t --rm -v $GOPATH/src/$REPO:/go/src/$REPO -v /var/run/docker.sock:/var/run/docker.sock --entrypoint bash quay.io/eris/eris /go/src/$REPO/DOCKER/eris-cli/run.sh
+docker run -t --rm --volumes-from eris-data -v /var/run/docker.sock:/var/run/docker.sock --entrypoint bash quay.io/eris/eris:$ERIS_VERSION /home/eris/.eris/mint-client/DOCKER/eris-cli/run.sh
