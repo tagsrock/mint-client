5f320d2908e20efce7a56f28493be8b04d4f85ef
Ethan Buchman <ethan@coinculture.info>
2015-12-03 18:01:46 +0000

    test cleanup

--- DOCKER/eris-cli/build.sh
+++ DOCKER/eris-cli/build.sh
@@ -18,3 +18,6 @@ docker cp $GOPATH/src/$REPO/ eris-data:/home/eris/.eris/mint-client/
 
 # eris/eris container in which we run the tests
 docker run -t --rm --volumes-from eris-data -v /var/run/docker.sock:/var/run/docker.sock --entrypoint bash quay.io/eris/eris:$ERIS_VERSION /home/eris/.eris/mint-client/DOCKER/eris-cli/run.sh
+
+# cleanup
+docker rm -vf eris-data
