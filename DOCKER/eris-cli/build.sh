613d42fdcf6cfc986e5ea6efbb29dcd1f37ee8c6
Casey Kuhlman <casey@erisindustries.com>
2016-02-01 22:28:55 +0100

    more updates of these integration tests

--- DOCKER/eris-cli/build.sh
+++ DOCKER/eris-cli/build.sh
@@ -4,12 +4,10 @@ set -e
 # mct = mint client test
 
 REPO=github.com/eris-ltd/mint-client
-if [[ "$ERIS_VERSION" == "" ]]; then
-	ERIS_VERSION=0.11
-fi
+cd $GOPATH/src/$REPO
 
-cd $GOPATH/src/github.com/eris-ltd/mint-client
 echo "********** BUILDING MINT-CLIENT ********"
+
 docker build -t mct_client -f ./DOCKER/linked/DockerfileClient . 
 
 docker run --name eris-data eris/data echo "Data-container for testing with eris-cli"
@@ -17,7 +15,7 @@ docker run --name eris-data eris/data echo "Data-container for testing with eris
 docker cp $GOPATH/src/$REPO/ eris-data:/home/eris/.eris/mint-client/
 
 # eris/eris container in which we run the tests
-docker run -t --rm --volumes-from eris-data -v /var/run/docker.sock:/var/run/docker.sock --entrypoint bash quay.io/eris/eris:$ERIS_VERSION /home/eris/.eris/mint-client/DOCKER/eris-cli/run.sh
+docker run -t --rm --volumes-from eris-data -v /var/run/docker.sock:/var/run/docker.sock --entrypoint bash quay.io/eris/eris:latest /home/eris/.eris/mint-client/DOCKER/eris-cli/run.sh
 
 # cleanup
 docker rm -vf eris-data
